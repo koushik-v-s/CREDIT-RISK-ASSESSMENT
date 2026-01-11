37a6a004d7e16416bb2649b3648b41fb32df12a0
koushik-v-s <148638606+koushik-v-s@users.noreply.github.com>
2026-01-11 11:50:21 +0530

    Delete validation.py

--- validation.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-
-def population_stability_index(expected, actual, bins=10):
-    expected_perc = pd.qcut(expected, bins, duplicates="drop").value_counts(normalize=True)
-    actual_perc = pd.qcut(actual, bins, duplicates="drop").value_counts(normalize=True)
-    psi = ((expected_perc - actual_perc) *
-           (expected_perc / actual_perc).apply(lambda x: np.log(x) if x > 0 else 0)).sum()
-    return psi
